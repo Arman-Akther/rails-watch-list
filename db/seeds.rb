b597d0672a6914895ac2e2c16e8c04268b8877b8
arman akther <armanakther@live.co.uk>
2023-02-23 14:19:21 +0000

    Seeds api complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# require 'faker'
+
+require 'open-uri'
+require 'json'
+
+url = "http://tmdb.lewagon.com/movie/top_rated"
+response = URI.open(url).read
+data = JSON.parse(response)
+movies_array = data["results"]
+puts "Creating table..."
+
+movies_array.each do |movie|
+  puts "Creating new movie..."
+  title = movie["title"]
+  overview = movie['overview']
+  poster_url = "https://image.tmdb.org/t/p/original#{movie['poster_path']}"
+  rating = movie['vote_average']
+  Movie.create(title: title, overview: overview, poster_url: poster_url, rating: rating)
+end
